ca9112ca31a8c4861b798dd269295f6f6ea82dca
user <email@email.email>
2025-07-29 22:44:26 +0200

    Improve logging
    * cmd/test_server/main.go:

--- cmd/test_server/main.go
+++ cmd/test_server/main.go
@@ -3,20 +3,34 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
 	"flag"
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+
+	"github.com/wilblik/proxyonthego/internal/log"
 )
 
 func main() {
 	port := flag.String("port", "8080", "Port for the http server to listen on")
+	quiet := flag.Bool("quiet", false, "Disable info logs")
 	flag.Parse()
 
+	if *quiet {
+		log.DisableInfo()
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		reqDump, err := httputil.DumpRequest(r, true)
+		if err != nil {
+			log.LogErr("Error dumping request: %v", err)
+		} else {
+			log.LogInfo("%s\n", string(reqDump))
+		}
 		fmt.Fprintln(w, "OK")
 	})
 
 	addr := fmt.Sprintf(":%s", *port)
-	log.Fatal(http.ListenAndServe(addr, nil))
+	log.LogInfo("Starting test server on %s", addr)
+	log.LogFatal(http.ListenAndServe(addr, nil))
 }
