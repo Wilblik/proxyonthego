f97912dd5c7e6397570ddf4ad7e08445d0528c91
user <email@email.email>
2025-07-29 22:42:58 +0200

    Add logging package

---
+++ internal/log/log.go
@@ -0,0 +1,32 @@
+package log
+
+import (
+	"log"
+	"os"
+	"io"
+)
+
+var (
+	infoLog = log.New(os.Stdout, "[INFO]", log.Ldate|log.Ltime)
+	errLog  = log.New(os.Stderr, "[ERROR] ", log.Ldate|log.Ltime)
+)
+
+func LogInfo(format string, v ...any) {
+	infoLog.Printf(format, v...)
+}
+
+func LogErr(format string, v ...any) {
+	errLog.Printf(format, v...)
+}
+
+func LogFatalf(format string, v ...any) {
+	errLog.Fatalf(format, v...)
+}
+
+func LogFatal(v ...any) {
+	errLog.Fatal(v...)
+}
+
+func DisableInfo() {
+	infoLog.SetOutput(io.Discard)
+}
